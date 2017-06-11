cef4dad005fc3b33bfc8d22bdfc44524fda66b53
jlovedean <jlovedean@gmail.com>
2017-06-11 15:41:17 -0400

    transfer commit

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var friends = ["Ariel", "Will", "Ryn", "Conner", "Eric"];
 
 for(var i = 0; i < friends.length; i++) {
-    console.log(friends[i]);
+    console.log(friends[i].toUpperCase() + ':');
     for(var a = 99; a > 0; a--) {
-        console.log(a + " lines of code in the file, " + a + " lines of code; " + friends[i] + " strikes out, clears it all out, " + (a-1) + " lines of code in the file.");
             if(a === 1) {
                 console.log(a + " line of code in the file, " + a + " line of code; " + friends[i] + "strikes one out, clears it all out, no more lines of code in the file");
+            } else if(a === 2) {
+                console.log(a + " lines of code in the file, " + a + " lines of code; " + friends[i] + " strikes out, clears it all out, " + (a-1) + " line of code in the file.");
+            } else {
+                console.log(a + " lines of code in the file, " + a + " lines of code; " + friends[i] + " strikes out, clears it all out, " + (a-1) + " lines of code in the file.");
             }
-}
+         }
 };
+
