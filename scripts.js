a12083b4a370034c04472ce272d6aa2686a2714a
jlovedean <jlovedean@gmail.com>
2017-08-17 13:49:10 -0400

    touch up commit

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ for(var i = 0; i < friends.length; i++) {
             } else if(a === 2) {
                 console.log(a + " lines of code in the file, " + a + " lines of code; " + friends[i] + " strikes out, clears it all out, " + (a-1) + " line of code in the file.");
             } else {
-                console.log(a + " lines of code in the file, " + a + " lines of code; " + friends[i] + " strikes out, clears it all out, " + (a-1) + " lines of code in the file.");
+                console.log(a + " lines of code in the file, " + a + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (a-1) + " lines of code in the file.");
             }
          }
 };
